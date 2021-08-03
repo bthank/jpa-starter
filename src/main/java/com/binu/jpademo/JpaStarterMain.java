5d7e70f759dcb8140b4ddb75732563802061ad4b
bthank <bthankachan@yahoo.com>
2021-08-03 00:08:11 -0400

    Added JPA persistence logic to add employees to db

--- src/main/java/com/binu/jpademo/JpaStarterMain.java
+++ src/main/java/com/binu/jpademo/JpaStarterMain.java
@@ -1,13 +1,38 @@
 package com.binu.jpademo;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaStarterMain {
 
 	
 	public static void main(String[] args) {
 		
-		Employee emp = new Employee();
-		emp.setId(1);
-		emp.setName("John Thompson");
+		Employee emp1 = new Employee();
+		emp1.setId(1);
+		emp1.setName("Phil French");
+
+		Employee emp2 = new Employee();
+		emp2.setId(2);
+		emp2.setName("Mary Smith");
+
+		Employee emp3 = new Employee();
+		emp3.setId(3);
+		emp3.setName("Paul Wilson");
+
+		
+		
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(emp1);
+		entityManager.persist(emp2);
+		entityManager.persist(emp3);
+		entityTransaction.commit();
 		
 		
 	}
