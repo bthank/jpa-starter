58d318d4545cb87a447ddb42c853a2da0420cf28
bthank <bthankachan@yahoo.com>
2021-08-02 23:42:09 -0400

    Initial commit

---
+++ src/main/java/com/binu/jpademo/Employee.java
@@ -0,0 +1,40 @@
+package com.binu.jpademo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity  // JPA annotation
+@Table(name="employee_data")
+public class Employee {
+
+	@Id
+	private int id;
+	
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
---
+++ src/main/java/com/binu/jpademo/JpaStarterMain.java
@@ -0,0 +1,18 @@
+package com.binu.jpademo;
+
+public class JpaStarterMain {
+
+	
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		emp.setId(1);
+		emp.setName("John Thompson");
+		
+		
+	}
+	
+	
+	
+	
+}
